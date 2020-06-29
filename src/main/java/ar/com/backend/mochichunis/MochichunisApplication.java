0101b3c7f05051cc0a74c7c2a560dc8aac774e31
Seba Ink <sebastian@gestioncervecera.com>
2020-06-28 22:45:58 -0300

    initial spring commit

---
+++ src/main/java/ar/com/backend/mochichunis/MochichunisApplication.java
@@ -0,0 +1,13 @@
+package ar.com.backend.mochichunis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MochichunisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MochichunisApplication.class, args);
+	}
+
+}
